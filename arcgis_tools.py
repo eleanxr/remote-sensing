40a54247072fb9f662c4236ab5a3241dbca630af
Will Dicharry <wdicharry@tnc.org>
2014-07-14 17:08:25 -0600

    Sum in separate areas.

--- arcgis_tools.py
+++ arcgis_tools.py
@@ -8,6 +8,8 @@ from matplotlib import pyplot as plt
 env.workspace = 'C:/Data/Grazing'
 env.overwriteOutput = True
 
+months = ["SEPT", "DEC", "MAR"]
+
 base_name = 'SEPT_B%d.TIF'
 
 inside_point = (351940.756, 3806355.54)
@@ -18,7 +20,7 @@ extractionType = "INSIDE"
 def make_point(t):
     return arcpy.Point(t[0], t[1])
 
-def compute_ndvi(red, nir):
+def compute_ndvi(red, nir, savename):
     
     def map_raster(raster):
         return Float(Raster(raster))
@@ -26,7 +28,7 @@ def compute_ndvi(red, nir):
     result = (map_raster(nir) - map_raster(red)) / (map_raster(nir) + map_raster(red))
     
     # Save the result for examination later.
-    result.save('ndvi')
+    result.save(savename)
     
     return result
     
@@ -34,21 +36,34 @@ def main():
     arcpy.CheckOutExtension("Spatial")
     RED = 4
     NIR = 5
-    
-    red_raster = base_name % RED
-    nir_raster = base_name % NIR
 
-    ndvi_raster = compute_ndvi(red_raster, nir_raster)
-    inside_circle = ExtractByCircle(ndvi_raster, make_point(inside_point), radius, extractionType)
-    inside_circle.save('out_inside')
-    outside_circle = ExtractByCircle(ndvi_raster, make_point(outside_point), radius, extractionType)
-    outside_circle.save('out_outside')
+    inside_sums = []
+    outside_sums = []
     
-    inside_array = arcpy.RasterToNumPyArray(inside_circle)
-    outside_array = arcpy.RasterToNumPyArray(outside_circle)
+    for month in months:
+        print "Begin month %s" % month
+        red_raster = '%s_B%d.TIF' % (month, RED)
+        nir_raster = '%s_B%d.TIF' % (month, NIR)
+    
+        print "Compute NDVI..."
+        ndvi_raster = compute_ndvi(red_raster, nir_raster, 'NDVI_%s' % month)
+        print "Extracting regions..."
+        inside_circle = ExtractByCircle(ndvi_raster, make_point(inside_point), radius, extractionType)
+        inside_circle.save('out_inside')
+        outside_circle = ExtractByCircle(ndvi_raster, make_point(outside_point), radius, extractionType)
+        outside_circle.save('out_outside')
+        
+        print "Compute sums..."
+        inside_array = arcpy.RasterToNumPyArray(inside_circle).ravel()
+        inside_sums.append(np.sum(inside_array))
+        outside_array = arcpy.RasterToNumPyArray(outside_circle).ravel()
+        outside_sums.append(np.sum(outside_array))
+        del inside_array
+        del outside_array
 
-    plt.hist(inside_array.ravel(), 256, [-1, 1])
-    plt.hist(outside_array.ravel(), 256, [-1, 1])
+    
+    plt.plot(inside_sums, color='blue')
+    plt.plot(outside_sums, color='red')
     plt.show()
 
 if __name__ == '__main__':

a7932e1b04f2525a2b24a7246c0dcbd87effc408
Will Dicharry <wdicharry@gmail.com>
2015-01-24 14:11:28 -0700

    Add init files for kernel packages.

---
+++ src/kernel/arcgis/__init__.py
@@ -0,0 +1,4 @@
+"""
+Kernel implementation that uses ArcGIS as the underlying
+provider for all GIS operations.
+"""
---
+++ src/kernel/gdal/__init__.py
@@ -0,0 +1,4 @@
+"""
+Kernel provider that uses GDAL for all underlying
+GIS operations.
+"""
